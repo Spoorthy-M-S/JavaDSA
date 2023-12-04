6b98d23fdaa0cb9b4540fd73b69a4c65f8668284
Spoorthy-M-S <142247032+Spoorthy-M-S@users.noreply.github.com>
2023-12-04 16:41:54 +0530

    Update ISelectionSort.java

--- Sorting/ISelectionSort.java
+++ Sorting/ISelectionSort.java
@@ -1,5 +1,35 @@
 package JavaDSA.Sorting;
 
 public class ISelectionSort {
+
+    public static void selectionSort(int arr[]){
+        for(int i=0;i<arr.length-1;i++){
+            int min=i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[min]>arr[j]){
+                    min=j;
+                }
+            }
+            int temp=arr[min];
+            arr[min]=arr[i];
+            arr[i]=temp;
+        }
+    }
+
+    public static void main(String args[]){
+        int arr[]={1,6,2,3,4,1};
+        System.out.println("Array before sorting:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        selectionSort(arr);
+        System.out.println("Array after sorting:");
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+
+    }
     
 }
+
