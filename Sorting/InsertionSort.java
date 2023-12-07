af31c0a8f1e06193f37ff09b02a45d7f3b5bd80e
Spoorthy-M-S <142247032+Spoorthy-M-S@users.noreply.github.com>
2023-12-07 12:57:24 +0530

    Update InsertionSort.java

--- Sorting/InsertionSort.java
+++ Sorting/InsertionSort.java
@@ -1,5 +1,29 @@
 package JavaDSA.Sorting;
+public class InsertionSort{
+  public static void inSort(int arr[]){
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            int j=i;
+            while(j>0 && arr[j-1]>arr[j]){
+                int temp=arr[j];
+                arr[j]=arr[j-1];
+                arr[j-1]=temp;
+                j--;
+            }
+        }
+    }
 
-public class InsertionSort {
+    public static void main(String args[]){
+        int arr[]={5,3,2,8,6};
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        inSort(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+
+    }
     
 }
