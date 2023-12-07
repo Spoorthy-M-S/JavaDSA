6b73721a20dd9472cac7e91579d474388ccc3869
Spoorthy-M-S <142247032+Spoorthy-M-S@users.noreply.github.com>
2023-12-07 12:56:15 +0530

    Update BubbleSort.java

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,5 +1,44 @@
 package JavaDSA.Sorting;
 
-public class BubbleSort {
-    
+import java.util.Arrays;
+
+public class BubbleSort{
+
+    public static void bbsort(int arr[]){
+        int n=arr.length;
+
+        for(int i=n-1;i>=1;i--){
+            for(int j=0;j<=i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    //swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                    
+                                    }
+                                    }
+                                }        }
+
+
+
+public static void main(String args[]){
+    int arr[]={1,5,3,9,5,3};
+    for(int i=0;i<arr.length;i++){
+    System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+    bbsort(arr);
+    for(int i=0;i<arr.length;i++){
+    System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+
+   
+    System.out.println("By in built function== Arrays.sort(Array_name)");
+    Arrays.sort(arr);
+    for(int i=0;i<arr.length;i++){
+    System.out.print(arr[i]+" ");
+    }
+    System.out.println();
+}    
 }
