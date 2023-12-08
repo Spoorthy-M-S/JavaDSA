2ce2c3840f0c344ad76937e6b4c6e79a1e2bc9e2
Spoorthy_M_S <spoorthyms462003@gmail.com>
2023-12-08 12:48:12 +0530

    Added program

---
+++ Arrays/LargestBrute.java
@@ -0,0 +1,12 @@
+package JavaDSA.Arrays;
+
+import java.util.*;
+public class LargestBrute{
+    public static void main(String args[]){
+        int arr[]={4,5,3,2,6};
+        Arrays.sort(arr);
+        System.out.println(arr[arr.length-1]);
+        
+    }
+
+}
